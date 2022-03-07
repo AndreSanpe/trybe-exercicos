3a8bf20f05776b878695ebbba229f6a28b7f3063
André Santos <x_sode@hotmail.com>
2022-03-07 20:14:23 -0300

    exercicios até o 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,65 @@
+// exercício 1
+
+// let a = 10;
+// let b = 11;
+
+// let adicao = (a + b);
+// console.log(adicao);
+
+
+// let subtracao = (a - b);
+// console.log(subtracao);
+
+// let multiplicacao = (a * b);
+// console.log(multiplicacao);
+
+// let divisao = (a / b);
+// console.log(divisao);
+
+// let modulo = (a % b);
+// console.log(modulo);
+
+// exercíco 2
+
+// const c = 20;
+// const d = 30;
+
+// if (c===d){
+//     console.log("As constantes são iguais!");
+// }
+// else{
+//     console.log("As constantes são diferentes");
+// }
+
+//exercício 3
+
+// const a = 10;
+// const b = 10;
+// const c = 10;
+
+// if (a === b && a === c){
+//     console.log("As constantes são iguais!");
+// }
+// else {
+//     console.log("As constantes não são iguais!");
+// }
+
+//exercício 4
+
+// const a = 1;
+
+// if (a > 0) {
+//     console.log("positivo");
+// }
+// else if (a == 0) {
+//     console.log("zero");
+
+// }
+// else {console.log("negativo")};
+
+//exercício 5
+
+const angA = 50;
+const angB = 70;
+const angC = 60;
+
