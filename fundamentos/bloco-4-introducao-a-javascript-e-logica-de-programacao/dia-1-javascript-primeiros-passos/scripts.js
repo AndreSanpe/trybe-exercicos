c94aa4455d6a217232d0d52d483dc85013d74653
André Santos <x_sode@hotmail.com>
2022-03-08 16:31:59 -0300

    Exercícios até o 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -21,29 +21,33 @@
 
 // exercíco 2
 
-// const c = 20;
-// const d = 30;
+// const c = 30;
+// const d = 20;
 
-// if (c===d){
-//     console.log("As constantes são iguais!");
+// if (c>d){
+//     console.log("C é maior que D");
+// }
+// else if (c<d){
+//     console.log("D é maior que C");
 // }
 // else{
-//     console.log("As constantes são diferentes");
+//     console.log("As constantes são iguais");
 // }
 
 //exercício 3
 
-// const a = 10;
-// const b = 10;
-// const c = 10;
 
-// if (a === b && a === c){
-//     console.log("As constantes são iguais!");
-// }
-// else {
-//     console.log("As constantes não são iguais!");
-// }
+// const a = 6;
+// const b = 4;
+// const c = 2;
 
+// if (a > b && a > c) {
+//   console.log('O maior número é: ' + a + ' (a)');
+// } else if (b > a && b > c) {
+//   console.log('O maior número é: ' + b + ' (b)');
+// } else {
+//   console.log('O maior número é: ' + c + ' (c)');
+// }
 //exercício 4
 
 // const a = 1;
@@ -59,13 +63,129 @@
 
 //exercício 5
 
-const angA = 50;
-const angB = 70;
-const angC = 70;
+// const angA = 50;
+// const angB = 70;
+// const angC = 70;
 
-if(angA + angB + angC == 180){
-    console.log("true");
-}
-else {
-    console.log("false");
-}
+// if(angA + angB + angC == 180){
+//     console.log("true");
+// }
+// else {
+//     console.log("false");
+// }
+
+//Exercício 6
+// nomeDaPaca = "pião";
+// let lowerCase = nomeDaPaca.toLowerCase();
+// console.log(lowerCase);
+
+// if (lowerCase == "bispo"){
+//     console.log("Diagonal livre");
+// }
+// else if (lowerCase == "cavalo"){
+//     console.log("Movimento em 'L', 2 e 3 casas");
+// }
+// else if (lowerCase == "torre"){
+//     console.log("Horizontal e Vertical livre");
+// }
+// else if (lowerCase == "rainha"){
+//     console.log("Todas as direções livres");
+// }
+// else if (lowerCase == "rei"){
+//     console.log("todas as direções 1 casa");
+// }
+// else if (lowerCase == "pião"){
+//     console.log("Primeiro movimento pode ser uma ou duas casas e todas as demais somente uma e só captura peças na diagonal")
+// }
+// else{
+//     console.log("Erro! Essa peça não existe no nosso banco de dados");
+// }
+
+// Exercício 7
+
+// let nota = 89;
+
+// if (nota >= 90 && nota <= 100){
+//     console.log("Nota A");
+// }
+// else if (nota >=80 && nota <90){
+//     console.log("Nota B");
+// }
+// else if (nota >=70 && nota <80){
+//     console.log("Nota C");
+// }
+// else if (nota >=60 && nota <70){
+//     console.log("Nota D");
+// }
+// else if (nota >=50 && nota <60){
+//     console.log("Nota E");
+// }
+// else if (nota <50 && nota > 0){
+//     console.log("Nota F");
+// }
+// else {
+//     console.log("Erro, nota não é possível de ser computada!!!");
+// }
+
+// Exercício 8
+// let A = 11;
+// let B = 21;
+// let C = 21;
+
+// if (A%2 === 0 || B%2 === 0 || C%2 === 0){
+//     console.log(true);
+// }
+// else {console.log(false);
+// }
+
+
+// ======================================================================= Exercicios dia 4.2! =======================\/
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu.indexOf("Portfólio");
+
+
+// console.log(menuServices);
+// menu.push('Contato');
+// console.log(menu);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for (let index = 0; index < groceryList.length; index++) {
+//     console.log("Comida:" + groceryList[index]);
+// }
+
+
+// let word = 'Hello';
+// for (let letter of word) {
+//   console.log(letter);
+// }
+// // resultado:
+// // "H"
+// // "e"
+// // "l"
+// // "l"
+// // "o"
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+//     for(let wichtName of names){
+//         console.log(wichtName);
+//     }
+// ===============================================Exercícios 4.2 ˆˆˆˆˆˆˆ======================
+// Exercicio 9
+// let A = 10;
+// let B = 20;
+// let C = 21;
+
+// if (A%2 != 0 || B%2 != 0 || C%2 != 0){
+//     console.log(true);
+// }
+// else {console.log(false);
+// }
+
+// Exercício 10
+
+const custoProduto = 100;
+const valorVenda = 200;
+
+let custoImposto = custoProduto*1.2;
+
+
