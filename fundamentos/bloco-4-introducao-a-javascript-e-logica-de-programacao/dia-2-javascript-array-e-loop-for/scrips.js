f38bd0cf37a504a40519df6d832505208b5328a3
André Santos <x_sode@hotmail.com>
2022-03-08 20:04:11 -0300

    Exercicio até o 10

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scrips.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scrips.js
@@ -1,46 +1,59 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 sum = 0;
-for(let i = 0; i <= numbers.length-1; i++){
+for (let i = 0; i <= numbers.length - 1; i++) {
     // console.log(numbers[i]);
     sum += numbers[i];
 
 }
 // console.log("soma: " + sum);
 let med = sum / numbers.length;
-console.log("Média: "+med);
+console.log("Média: " + med);
 
-if (med <= 20){
+if (med <= 20) {
     console.log("Valor é menor que 20!");
 }
-else{
+else {
     console.log("Valor é maior que 20");
 }
-console.log("O número de números é: "+numbers.length);
+console.log("O número de números é: " + numbers.length);
 
 let maiorValor = numbers[0];
 
-for(let index = 1; index < numbers.length; index++){
-    if(numbers[index]>maiorValor){
+for (let index = 1; index < numbers.length; index++) {
+    if (numbers[index] > maiorValor) {
         maiorValor = numbers[index];
     }
 }
-console.log("O maior Valor é: "+ maiorValor);
+console.log("O maior Valor é: " + maiorValor);
 
 let compareNum = numbers[0];
 let oddNum = 0;
 
-for (let i = 0; i < numbers.length; i++){
-    if (numbers[i]%2 !== 0){
-oddNum++
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+        oddNum++
     }
 }
-console.log("O número de números ímpares é: "+oddNum);
+console.log("O número de números ímpares é: " + oddNum);
 
 let smolestValue = numbers[0];
 
-for (let i = 0; i < numbers.length; i++){
-    if (numbers[i] < smolestValue){
-    smolestValue=numbers[i];    
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < smolestValue) {
+        smolestValue = numbers[i];
     }
 }
-console.log("O menor valor é: "+ smolestValue);
+console.log("O menor valor é: " + smolestValue);
+
+let array = [];
+let arrayDiv = [];
+for (let i = 1; i <= 25; i++) {
+    array.push(i);
+}
+console.log("O array formado é: ", array);
+
+for (let i = 0; i < array.length; i++) {
+    arrayDiv.push (array[i] / 2);
+}
+console.log("A array com os termos divididos é: ", arrayDiv);
+
