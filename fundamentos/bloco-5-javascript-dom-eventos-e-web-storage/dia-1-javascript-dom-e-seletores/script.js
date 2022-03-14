8191b5ecc46b1ae9b34231bbb25715d0952f0889
André Santos <x_sode@hotmail.com>
2022-03-14 17:48:35 -0300

    Exercicios aula assincrona

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,11 @@
+const header = document.getElementById('header-container');
+header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+const div = document.getElementsByClassName('emergency-tasks')[0];
+div.style.backgroundColor = 'rgb(222, 176, 105)';
+
+const noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks')[0];
+noEmergencyTasks.style.backgroundColor = 'rgb(222,222,222)';
+
+const footerContainer = document.getElementById('footer-container');
+footerContainer.style.backgroundColor = 'rgb(111,111,111)';
