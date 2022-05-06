d335bdf5f6b6e8a8073bb1479dc7d688208cd616
André Santos <x_sode@hotmail.com>
2022-05-06 11:26:46 -0300

    Fim primeiro dia front-end

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio3/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio3/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
       <Header/>
       <Content/>
+      <Footer/>
       </header>
       <main>
       </main>
