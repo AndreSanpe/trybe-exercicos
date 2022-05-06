f715911643cb3b66659ecfbe373b097c57b431d5
André Santos <x_sode@hotmail.com>
2022-05-06 11:21:24 -0300

    ex3

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio3/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio3/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Content from './Content';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Header/>
+      <Content/>
       </header>
+      <main>
+      </main>
     </div>
   );
 }
