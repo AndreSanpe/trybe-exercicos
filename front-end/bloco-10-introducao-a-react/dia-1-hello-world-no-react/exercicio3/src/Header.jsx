7225387abe77c80adee444d1e736db76a022ab2e
André Santos <x_sode@hotmail.com>
2022-05-06 11:04:11 -0300

    Ex3

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio3/src/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+
+            <>
+
+                <h1>Conteudo de front-end</h1>
+            </>
+        )
+    }
+}
+
+export default Header;
